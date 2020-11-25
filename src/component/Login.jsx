3a69b7d2b254d00f8d4ca9d759ef475c3da2af11
bagus priambudi <baguspriambudi77@gmail.com>
2020-11-25 15:55:38 +0700

    add: Login

---
+++ src/component/Login.jsx
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+
+const Login = ()=>{
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onChangeUsername= (val)=>{
+        const value = val.target.value
+        setUsername(value)
+    }
+
+    const onChangePassword= (val)=>{
+        const value = val.target.value
+        setPassword(value)
+    }
+
+    return(
+        <div style={{marginTop: "125px"}}>
+            <div className="container">
+                <div className="row justify-content-center">
+                   <div className="col-md-6">
+                       <div className="card p-4">
+                           <div className="card-body">
+                                <div className="form-group">
+                                    <label>Username</label>
+                                    <input placeholder='Username' className="input form-control" value={username} onChange={onChangeUsername}></input>
+                                </div>
+                                
+                                <div className="form-group">
+                                    <label>Password</label>
+                                    <input placeholder='Password' className="input form-control" value={password} onChangePassword={onChangePassword}></input>
+                                </div>
+                                <button className='btn btn-success'>Login</button>
+                           </div>
+                       </div>
+                   </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Home from './container/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );

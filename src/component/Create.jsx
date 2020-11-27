5e0561cd965a0e2b495981c72a9591dc7ace1a4f
bagus priambudi <baguspriambudi77@gmail.com>
2020-11-27 09:47:51 +0700

    add: state in create.jsx

--- src/component/Create.jsx
+++ src/component/Create.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Create = () =>{
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
+
+    const ChangeUsername= (val)=>{
+        const value = val.target.value
+        setUsername(value)
+    }
+
+    const ChangePassword= (val)=>{
+        const value = val.target.value
+        setPassword(value)
+    }
+
    return(
     <div style={{marginTop: "125px"}}>
     <div className="container">
@@ -10,14 +23,14 @@ const Create = () =>{
                    <div className="card-body">
                         <div className="form-group">
                             <label>Username</label>
-                            <input type='text' placeholder='Username' className="input form-control"></input>
+                            <input type='text' placeholder='Username' className="input form-control" value={username} onChange={ChangeUsername}></input>
                         </div>
                         
                         <div className="form-group">
                             <label>Password</label>
-                            <input type='password' placeholder='Password' className="input form-control"></input>
+                            <input type='password' placeholder='Password' className="input form-control" value={password} onChange={ChangePassword}></input>
                         </div>
-                        <button className='btn btn-success'>Login</button>
+                        <button className='btn btn-success'>Create</button>
                    </div>
                </div>
            </div>

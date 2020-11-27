38748c41477501e40ed5baab7ad9f5473d0412c8
bagus priambudi <baguspriambudi77@gmail.com>
2020-11-27 15:02:34 +0700

    add: method post using axios

--- src/component/Create.jsx
+++ src/component/Create.jsx
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 const Create = () =>{
     const [username,setUsername] = useState('');
     const [password,setPassword] = useState('');
+    const [alert,setAlert] = useState('');
 
     const ChangeUsername= (val)=>{
         const value = val.target.value
@@ -15,6 +17,25 @@ const Create = () =>{
         setPassword(value)
     }
 
+    const ClickCreate = () =>{
+       const data = {
+           username : username,
+           password : password
+       }
+      axios.post('https://bootcamp-imogiri.herokuapp.com/api/v1/auth/user/create', data).then(result =>{
+          if(result){
+              if(result.data){
+                  setUsername('');
+                  setPassword('');
+                  setAlert(result.data.message);
+                  setTimeout(()=>{
+                      setAlert('')
+                  }, 3000)
+              }
+          }
+      })
+    }
+
    return(
     <div style={{marginTop: "125px"}}>
     <div className="container">
@@ -23,6 +44,13 @@ const Create = () =>{
                <div className="card p-4">
                     <h2>Create Account</h2>
                    <div className="card-body">
+                       {
+                           alert && (
+                            <div className='alert alert-success'>
+                                <p>{alert}</p>
+                            </div>
+                           )
+                       }
                         <div className="form-group">
                             <label>Username</label>
                             <input type='text' placeholder='Username' className="input form-control" value={username} onChange={ChangeUsername}></input>
@@ -32,7 +60,7 @@ const Create = () =>{
                             <label>Password</label>
                             <input type='password' placeholder='Password' className="input form-control" value={password} onChange={ChangePassword}></input>
                         </div>
-                        <button className='btn btn-success'>Create</button> { }
+                        <button className='btn btn-success' onClick={ClickCreate}>Create</button> { }
                         <Link to='/' className='btn btn-danger'>Cancel</Link>
                    </div>
                </div>
